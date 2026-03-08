2d98a05c50df988503d8ef1e4339cc7d4bfab99f
kinguser981@gmail.com <Samuel Kendall>
2026-03-08 00:02:51 +0000

    TWRP device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_moon.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_moon-user \
+    omni_moon-userdebug \
+    omni_moon-eng
